4976d773ded75ba437e631d08a9abfca21b2b8bb
Scarlett Kim <scarlettkimm@gmail.com>
2020-01-13 19:06:49 -0500

    add composer file

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+//this is our controller
+//turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+//require autoload file
+require_once('vendor/autoload.php');
+
+//create instance of the base class
+$f3 = Base::instance();
+
+//define a default route
+$f3->route('GET /', function(){
+    $view = new Template();
+    echo $view->render('views/index.html');
+});
+//run fat free
+$f3->run();
